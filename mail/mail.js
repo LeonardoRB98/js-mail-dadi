3029b7d74f979d20c2045700027c47e28de439a9
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-12 16:58:56 +0100

    modifiche es mail

--- mail/mail.js
+++ mail/mail.js
@@ -1,16 +1,22 @@
 // chiedi all'utente la sua mail
-var mailUtente = prompt("inserisci la tua mail");
-console.log("Mail Utente: " + mailUtente);
+var mailUtente = prompt("LA TUA MAIL");
+console.log(mailUtente);
 // crea una mail list
-var mailList = ["leonardo@gamail.com", "andrea@gamail.com", "marco@gamail.com"]
-console.log("Mail Array: " + mailList)
+var listaMail = ['mail1@gmail.com', 'mail2@gmail.com', 'mail3@gmail.com']
+console.log(listaMail);
 // verifica che la mail inserita dall'utente sia tra le mail inserite nella mail list
-for (var i = 0; i < mailList.lenght; i++) {
-  console.log(mailList[i]);
+for (var i = 0; i < listaMail.length; i++) {
+  console.log(listaMail[i]);
+  if (listaMail == mailUtente) {
+    var result = true;
+  }else {
+    result = false;
+  }
+  console.log(result);
+}
+if (result == true) {
+  alert('sei nella lista');
+}else {
+  alert('non sei nella lista');
 }
 // stampa un messaggio appropriato in base all'email inserita
-if (mailList[i] == mailUtente) {
-  alert("sei nella lista")
-}else {
-  alert("non sei nella lista")
-}
